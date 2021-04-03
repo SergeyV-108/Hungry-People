3ba41b4a8f7a81f27217dc70d23b995a63e4c7f9
SergeyV-108 <79102071+SergeyV-108@users.noreply.github.com>
2021-04-03 13:19:42 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+// Инициализируем слайдер
+
+var swiper = new Swiper('.specialties-page__container', {
+	//Буллеты
+	pagination: {
+		el: '.specialties-page__pagination',
+		clickable: true,
+	},
+
+	//Бесконечный цикл
+	//loop: true,
+
+});
